55ec1ae62a845b2135b01848b7c2d3cb597a04b8
hyein You <laksjd4560@gmail.com>
2022-08-24 16:09:17 +0900

    모든 옵션 선택 X -> '생성하기' Button 클릭시 Toast 띄우기

--- app/src/main/java/com/example/making_a_habit/view/CreatinghabitActivity.kt
+++ app/src/main/java/com/example/making_a_habit/view/CreatinghabitActivity.kt
@@ -1,6 +1,9 @@
 package com.example.making_a_habit.view
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
@@ -34,9 +37,30 @@ class CreatinghabitActivity : AppCompatActivity() {
         }
 
 
+        /***** editText 20자 초과시 Toast 띄우기 *****/
+        // ToDo Toast 메시지 디자인 하기
+        binding.habitNameEdittext.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(h: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            } // 텍스트가 변경된 이후에 동작
+
+            override fun onTextChanged(h: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            } // 텍스트가 변경되기 바로 이전에 동작
+
+            override fun afterTextChanged(h: Editable?) {
+                if(h != null && !h.toString().equals("")){
+                    if(h.toString().length == 20){
+                        val habitName20Up = "습관 이름은 20자까지만 가능합니다!"
+                        Toast.makeText(applicationContext, habitName20Up, Toast.LENGTH_SHORT).show()
+                    }
+                }
+            } // 텍스트가 변경되는 동시에 동작
+        })
+
+
         /***** habitPeriod_btn 클릭하면 버튼눌림효과 유지 *****/
         // 기간 눌림효과, 눌림효과 해제  횟수 눌림효과, 눌림효과 해제
         // 버튼 누른 상태에서 다른 버튼 누르면 눌림효과 해제 후 눌림
+        // TODO 여기 있는 모든 코드들이 구림. 정말 이게 최선...?
 
         // 기간 Button
         binding.habitPeriodBtnTime.setOnClickListener{
@@ -103,8 +127,21 @@ class CreatinghabitActivity : AppCompatActivity() {
             val habit = Habit(id, habitName, habitPeriod.toString(), hebitPeriodNum,"임시", habitDateStart.toString(),
                     "임시", 0, false, "임시")
             println("Habit : " + habit)
-            habitViewModel.insert(habit)
-            finish()
+
+            // and &&   or ||
+            if(binding.habitNameEdittext.isEnabled &&
+                (binding.habitPeriodBtnTime.isSelected || binding.habitPeriodBtnNumber.isSelected) &&
+                (binding.habitPeriodNumBtn3.isSelected || binding.habitPeriodNumBtn15.isSelected || binding.habitPeriodNumBtn30.isSelected)
+            ){  // TODO 조건이 너무 구려 어떻게 짧게 할 수 없나?
+                habitViewModel.insert(habit)
+                finish()
+            }
+            else{
+                val creatinghabitbutton = "모든 옵션을 선택해주세요."
+                Toast.makeText(applicationContext, creatinghabitbutton, Toast.LENGTH_SHORT).show()
+            }
+
+
         }
 
 
@@ -116,24 +153,4 @@ class CreatinghabitActivity : AppCompatActivity() {
 
     } // onCreate
 
-    // TODO editText 토스트 메세지 띄우기
-    /*
-    fun abc(){
-        habitName_edittext.addTextChangedListener(object : TextWatcher{
-            override fun beforeTextChanged(h: CharSequence?, p1: Int, p2: Int, p3: Int) {
-            } // 텍스트가 변경된 이후에 동작
-
-            override fun onTextChanged(h: CharSequence?, p1: Int, p2: Int, p3: Int) {
-            } // 텍스트가 변경되기 바로 이전에 동작
-
-            override fun afterTextChanged(h: Editable?) {
-
-                if(h != null && !h.toString().equals("")){
-                    //if(h.toString().toByte().length <= 20)
-                }
-
-            } // 텍스트가 변경되는 동시에 동작
-        })
-    }
-     */
 }
