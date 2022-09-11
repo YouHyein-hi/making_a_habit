65f2f055903187c72d48f9353b4045561368217e
hyein You <laksjd4560@gmail.com>
2022-09-11 20:12:50 +0900

    HabitDB version 업데이트

--- app/src/main/java/com/example/making_a_habit/model/HabitDB.kt
+++ app/src/main/java/com/example/making_a_habit/model/HabitDB.kt
@@ -5,7 +5,7 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = [Habit::class], version = 3)
+@Database(entities = [Habit::class], version = 4)
 abstract class HabitDB : RoomDatabase() {
     abstract fun habitDao(): HabitDAO
 
