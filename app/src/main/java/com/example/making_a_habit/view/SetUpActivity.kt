7451653818f5002f43fd6b2f4c29f67175fc1e44
hyein You <laksjd4560@gmail.com>
2022-09-12 15:24:08 +0900

    오픈소스 라이선스 작업

--- app/src/main/java/com/example/making_a_habit/view/SetUpActivity.kt
+++ app/src/main/java/com/example/making_a_habit/view/SetUpActivity.kt
@@ -7,6 +7,8 @@ import androidx.appcompat.app.AppCompatActivity
 import com.example.making_a_habit.R
 import com.example.making_a_habit.databinding.ListDonehabitPageBinding
 import com.example.making_a_habit.databinding.SetupPageBinding
+import com.google.android.gms.oss.licenses.OssLicensesMenuActivity
+import com.google.android.gms.oss.licenses.OssLicensesMenuActivity.setActivityTitle
 
 class SetUpActivity : AppCompatActivity() {
 
@@ -21,6 +23,14 @@ class SetUpActivity : AppCompatActivity() {
 
         binding.backBtnSetuppage.setOnClickListener{
             finish()
+        }
+
+        // TODO 오픈소스라이선스 부분 해결하기 (소히나 푸름님한테 물어볼까?)
+        binding.opensourceLayout.setOnClickListener { it ->
+            Intent(applicationContext, OssLicensesMenuActivity::class.java).also {
+                startActivity(it)
+            }
+            true
         }
     }
 
